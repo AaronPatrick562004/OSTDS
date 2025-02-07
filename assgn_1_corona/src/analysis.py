4065911c1dc98d9b3020b93e6559c0e542a7a6e1
Aaron Patrick <aaronpatrick572004@gmail.com>
2025-02-07 15:12:57 +0530

    Create analysis.py

---
+++ assgn_1_corona/src/analysis.py
@@ -0,0 +1,4 @@
+# TODO extract only US data
+# TODO correlation = cleaned_data.corr() 
+# TODO omit long,lat,fix correlation, get statewise summary data, get insight into deaths, recovered, case fatality ratio
+# TODO exception handling, relative path, logging(logging library)(set log level), output beautificaiton and visual dashboard
